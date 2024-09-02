6011a0714a552f9c72276da457cd8d43dcfa2e82
Muhammad Fakhar-e-Iqbal Abdullah <fakhareiqbal3534@gmail.com>
2024-09-03 01:31:24 +0500

    signup ui

--- lib/features/auth/presentation/pages/sign_up.dart
+++ lib/features/auth/presentation/pages/sign_up.dart
@@ -1,4 +1,6 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -11,19 +13,36 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
+    return  Scaffold(
+      body: Padding( 
+        padding: const EdgeInsets.all(15.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Sign Up.', style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold),),
-            SizedBox(height: 30,),
-            AuthField(hintText: 'Name...',),
-            SizedBox(height: 15,),
-            AuthField(hintText: 'abc@gmail.com',),
-            SizedBox(height: 15,),
-            AuthField(hintText: 'Password',),
+            const Text('Sign Up.', style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold),),
+            const SizedBox(height: 30,),
+            const AuthField(hintText: 'Name...',),
+            const SizedBox(height: 15,),
+            const AuthField(hintText: 'abc@gmail.com',),
+            const SizedBox(height: 15,),
+            const AuthField(hintText: 'Password',),
+            const SizedBox(height: 20,),
+            const AuthGradientButton(),
+            const SizedBox(height: 20,),
+            RichText(text: TextSpan(
+            text: "Don't have an account?  ",
+            style: Theme.of(context).textTheme.titleMedium,
+            children: [
+              TextSpan(
+                text: 'Sign In',
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                  color: AppPallete.gradient2,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+            ),
+            ),
           ],
         ),
       ),
